933ceb7260cccd83fbe03d65dc000e84f34f4715
pso11 <sofiya.pshenichnaya.09@mail.ru>
2026-09-18 15:46:13 +0300

    Готовая стортировка с одним буфером

--- quick_sort.c
+++ quick_sort.c
@@ -4,11 +4,8 @@
 #include <assert.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys\stat.h>
 
-#define MAX_LINES 731
-#define MAX_LEN_LINE 100
-
-int  compare_numbers(const void* data, const void* reference_data);
 int  alphabetic_compare_lines(const void* data, const void* reference_data);
 int  rhymed_compare_lines(const void* data, const void* reference_data);
 
@@ -17,25 +14,30 @@ void print_lines_array(const char** lines_array, size_t line_elements);
 void swap(void* data, void* next_data, size_t type_size);
 void* create_reference_point(void* array, size_t number_elements, size_t type_size);
 void free_reference_point(void* reference_pointer);
-void write_file(const char** lines_array);
-const char** read_file(void);
-const char** copy_array(const char** lines_array);
-void  free_all_arrays(char** lines_array, char** copied_lines_array);
+
+void write_sorted_file(const char** lines_array, size_t lines_number);
+void write_poem_file(const char* elements_array, size_t lines_number);
+const char* read_file(void);
+const char** create_lines_array(char* elements_array, size_t lines_number);
+size_t count_lines(const char* elements_array);
+void  free_all_arrays(char** lines_array, char* elements_array, size_t lines_number);
 
 int main(void)
 {
-    const char** lines_array = read_file();
-    const char** copied_lines_array = copy_array(lines_array);
+    const char* elements_array = read_file();
+    assert(elements_array != NULL);
+    size_t lines_number = count_lines(elements_array);
+    const char** lines_array = create_lines_array((char*)elements_array, lines_number);
 
-    my_qsort(lines_array, MAX_LINES, sizeof(char*), &rhymed_compare_lines);
-    write_file(lines_array);
+    my_qsort(lines_array, lines_number, sizeof(char*), &rhymed_compare_lines);
+    write_sorted_file(lines_array, lines_number);
 
-    my_qsort(lines_array, MAX_LINES, sizeof(char*), &alphabetic_compare_lines);
-    write_file(lines_array);
+    my_qsort(lines_array, lines_number, sizeof(char*), &alphabetic_compare_lines);
+    write_sorted_file(lines_array, lines_number);
 
-    write_file(copied_lines_array);
+    write_poem_file(elements_array, lines_number);
 
-    free_all_arrays((char**)lines_array, (char**)copied_lines_array);
+    free_all_arrays((char**)lines_array, (char*)elements_array, lines_number);
 
     return 0;
 }
@@ -58,8 +60,11 @@ void my_qsort(void* array, size_t number_elements, size_t type_size, int (*compa
         }
 
         while (compare_function((unsigned char*)array + type_size * right_idx, reference_pointer) > 0)
+        {
             right_idx--;
-
+            //assert(right_idx >= 0);
+        }
+        //assert(left_pointer < right_pointer);
         if (left_idx <= right_idx)
         {
             swap((unsigned char*)array + type_size * left_idx, (unsigned char*)array + type_size * right_idx, type_size);
@@ -94,11 +99,6 @@ void swap(void* data, void* next_data, size_t type_size)
     }
 }
 
-int compare_numbers(const void* data, const void* reference_data)
-{
-    return (*(const int*)data - *(const int*)reference_data);
-}
-
 int alphabetic_compare_lines(const void* data, const void* reference_data)
 {
     const char* str1 = *(const char**)data;
@@ -107,7 +107,7 @@ int alphabetic_compare_lines(const void* data, const void* reference_data)
     int i_1 = 0;
     int i_2 = 0;
 
-    while (str1[i_1] != '\0' || str2[i_2] != '\0')
+    while (str1[i_1] != '\n' || str2[i_2] != '\n')
     {
         int c1 = tolower(str1[i_1]);
         int c2 = tolower(str2[i_2]);
@@ -146,8 +146,8 @@ int rhymed_compare_lines(const void* data, const void* reference_data)
 
     while (i_1 >= 0 && i_2 >= 0)
     {
-        int c1 = tolower(str1[i_1]);
-        int c2 = tolower(str2[i_2]);
+        size_t c1 = tolower(str1[i_1]);
+        size_t c2 = tolower(str2[i_2]);
 
         if (!ispunct(c1) && !ispunct(c2))
         {
@@ -183,74 +183,111 @@ void* create_reference_point(void* array, size_t number_elements, size_t type_si
 void free_reference_point(void* reference_pointer)
 {
     free(reference_pointer);
-}
+};
 
-const char** read_file(void)
+const char* read_file(void)
 {
-    char** lines_array = (char**)calloc(MAX_LINES, sizeof(char*));
 
     FILE* file = fopen("onegin_english.txt", "r");
     if (file == NULL)
     {
         printf("Can't open file");
-        return 0;
+        return NULL;
     }
 
-    char buffer[MAX_LEN_LINE] = {};
+    struct stat buff = {};
+    fstat(fileno(file), &buff);
 
-    for (size_t i = 0; i < MAX_LINES; i++)
-    {
-        int ch = 0;
-        size_t y = 0;
-        while ((ch = fgetc(file)) != EOF && ch != '\n')
-        {
-            buffer[y] = (char)ch;
-            y++;
-        }
-        buffer[y] = '\n';
-        buffer[y + 1] = '\0';
+    size_t wrong_number_elements = buff.st_size;
 
-        lines_array[i] = (char*)calloc(strlen(buffer) + 1, sizeof(char));
-        strncpy(lines_array[i], buffer, strlen(buffer) + 1);
-    }
+    char* elements_array = (char*)calloc(wrong_number_elements, sizeof(char));
+
+    size_t real_number_elements = fread(elements_array, sizeof(char), wrong_number_elements, file);
+
+    elements_array[real_number_elements] = '\0';
 
     fclose(file);
+
+    return (const char*)elements_array;
+}
+
+size_t count_lines(const char* elements_array)
+{
+    size_t i = 0, lines_number = 0;
+    for (; elements_array[i] != '\0'; i++)
+    {
+        if (elements_array[i] == '\n')
+            lines_number++;
+    }
+    if (elements_array[i - 1] != '\n')
+        lines_number++;
+
+    return lines_number;
+}
+
+void write_sorted_file(const char** lines_array, size_t lines_number)
+{
+
+    FILE* file = fopen("mc_onegin.txt", "a");
+    for (size_t i = 0; i < lines_number; i++)
+    {
+        fprintf(file, "%s", lines_array[i]);
+        fputc('\n', file);
+    }
+    fputc('\n', file);
+
+    fclose(file);
+}
+
+const char** create_lines_array(char* elements_array, size_t lines_number)
+{
+    char** lines_array = (char**)calloc(lines_number, sizeof(char*));
+    lines_array[0] = elements_array;
+    for(size_t i = 0, j = 1; elements_array[i] != '\0'; i++)
+    {
+        if (elements_array[i] == '\n')
+        {
+            elements_array[i] = '\0';
+            lines_array[j] = elements_array + i + 1;
+            j++;
+        }
+    }
 
     return (const char**)lines_array;
 }
 
-void write_file(const char** lines_array)
+void free_all_arrays(char** lines_array, char* elements_array, size_t lines_number)
 {
-    FILE* file = fopen("mc_onegin.txt", "a");
-    for (size_t i = 0; i < MAX_LINES; i++)
-    {
-        fprintf(file, "%s", lines_array[i]);
-    }
-    fprintf(file, "\n");
-    fclose(file);
-}
-
-const char** copy_array(const char** lines_array)
-{
-    char** copied_lines_array = (char**)calloc(MAX_LINES, sizeof(char*));
-
-    for (int i = 0; i < MAX_LINES; i++)
-    {
-        copied_lines_array[i] = (char*)calloc(strlen(lines_array[i]) + 1, sizeof(char*));
-
-        strncpy(copied_lines_array[i], lines_array[i], strlen(lines_array[i]) + 1);
-    }
-
-    return (const char**)copied_lines_array;
-}
-
-void  free_all_arrays(char** lines_array, char** copied_lines_array)
-{
-    for (int i = 0; i < MAX_LINES; i++)
+    for (size_t i = 0; i < lines_number; i++)
         free(lines_array[i]);
     free(lines_array);
 
-    for (int i = 0; i < MAX_LINES; i++)
-        free(copied_lines_array[i]);
-    free(copied_lines_array);
+    free(elements_array);
+}
+
+void print_lines_array(const char** lines_array, size_t line_elements)
+{
+    for (size_t i = 0; i < line_elements; i++)
+    {
+        printf("<%s>", *(lines_array + i));
+        putchar('\n');
+    }
+
+    putchar('\n');
+}
+
+void write_poem_file(const char* elements_array, size_t lines_number)
+{
+    FILE* file = fopen("mc_onegin.txt", "a");
+
+    const char* ptr = elements_array;
+    for (size_t i = 0; i < lines_number + 2; i++)
+    {
+        fprintf(file, "%s", ptr);
+        fputc('\n', file);
+        ptr = strchr(ptr, 0);
+        ptr++;
+    }
+
+    fclose(file);
 }
